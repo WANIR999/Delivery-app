3e43849a7a079fcc05fef74d5d09bbd66b217487
mohammed <mohammedwanir67@gmail.com>
2022-12-05 10:08:25 +0100

    MA-9 front m-pattern updates

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,14 +1,34 @@
-// import { useState } from "react";
-// import "../app.css";
-// import FormInput from "./Forminput";
-// import { useNavigate, Link } from "react-router-dom";
-// import axios from 'axios'
+import { useState } from "react";
+import "../app.css";
+import FormInput from "./Forminput";
+import { useNavigate, Link } from "react-router-dom";
+import axios from 'axios'
 
 const Login = () => {
 
   return (
-    <div >
-     
+    <div className="App auth fill">
+      <form>
+        <h1>Login</h1>
+        {/* {errmsg ? (
+          <div className="alert alert-danger text-center" role="alert">{errmsg}</div>
+        ):msg? (
+          <div className="alert alert-success text-center" role="alert">{msg}</div>
+        ):""} */}
+        {/* {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))} */}
+        <button>Submit</button>
+        <div className="w-100 d-flex justify-content-between align-items-center pb-3">
+        <Link to="/register"  className="btn text-secondary" >sign-up?</Link>
+        <Link to="/forgetpassword"  className="btn text-secondary" >forget ur password?</Link>
+        </div>
+      </form>
     </div>
   );
 };
--- frontend/src/components/Reset_password.jsx
+++ frontend/src/components/Reset_password.jsx
@@ -1,14 +1,30 @@
-// import { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import "../app.css";
-// import FormInput from "./Forminput";
-// import axios from 'axios'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../app.css";
+import FormInput from "./Forminput";
+import axios from 'axios'
 
 const Reset = () => {
   return (
-    <div>
-      
-    </div>
+    <div className="App ">
+    <form  className="bgf">
+      <h1>Reset password</h1>
+      {/* {errmsg ? (
+        <div className="alert alert-danger text-center" role="alert">{errmsg}</div>
+      ):msg? (
+        <div className="alert alert-success text-center" role="alert">{msg}</div>
+      ):""} */}
+      {/* {inputs.map((input) => (
+        <FormInput
+          key={input.id}
+          {...input}
+          value={values[input.name]}
+          onChange={onChange}
+        />
+      ))} */}
+      <button>Submit</button>
+    </form>
+  </div>
   );
 };
 
