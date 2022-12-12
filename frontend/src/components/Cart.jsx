fdfd5111d52212962301f1b9d08f03e20946ecfd
achnitreda <achnitreda@gmail.com>
2022-12-12 11:57:51 +0100

    add Cart component

---
+++ frontend/src/components/Cart.jsx
@@ -0,0 +1,14 @@
+const Cart = () => {
+    const Cart = [
+        {
+          id: "u1",
+          image: "https://upload.wikimedia.org/wikipedia/commons/4/47/Hamburger_%28black_bg%29.jpg",
+          name: "Burgers",
+        }
+      ];
+  return <>
+   <h2>Cart</h2>
+  </>;
+};
+
+export default Cart;
