a27a3b5228db43317b9a41643deedd2896a33f4e
achnitreda <achnitreda@gmail.com>
2022-12-23 11:27:35 +0100

    add and edit on createCheckoutSession

--- backend/controller/paymentController.js
+++ backend/controller/paymentController.js
@@ -8,22 +8,80 @@ const GetAllPayments= async(req,res)=>{
    }
 
 const createCheckoutSession = async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name: 'T-shirt',
+  const line_items = req.body.cartItems.map((item) => {
+    return {
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.name,
+          images: [item.image],
+          description: item.desc,
+          metadata: {
+            id: item.id,
           },
-          unit_amount: 2000,
         },
-        quantity: 1,
+        unit_amount: item.price * 100,
+      },
+      quantity: item.cartQuantity,
+    };
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    shipping_address_collection: {
+      allowed_countries: ["US", "MA", "MR"],
+    },
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: {
+            amount: 0,
+            currency: "usd",
+          },
+          display_name: "Free shipping",
+          // Delivers between 5-7 business days
+          delivery_estimate: {
+            minimum: {
+              unit: "business_day",
+              value: 5,
+            },
+            maximum: {
+              unit: "business_day",
+              value: 7,
+            },
+          },
+        },
+      },
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: {
+            amount: 1500,
+            currency: "usd",
+          },
+          display_name: "Next day air",
+          // Delivers in exactly 1 business day
+          delivery_estimate: {
+            minimum: {
+              unit: "business_day",
+              value: 1,
+            },
+            maximum: {
+              unit: "business_day",
+              value: 1,
+            },
+          },
+        },
       },
     ],
+    phone_number_collection: {
+      enabled: true,
+    },
+    line_items,
     mode: 'payment',
-    success_url: 'http://localhost:3000/checkout-success',
-    cancel_url: 'http://localhost:3000/cart',
+    success_url: 'http://localhost:3000/auth/checkout-success',
+    cancel_url: 'http://localhost:3000/auth/cart',
   });
 
   /* Sending the session url to the client. */
